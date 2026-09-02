494ca5853c53c9860f6d1af6d55a99907e15419e
danxczm <danbohdan92@gmail.com>
2026-09-02 07:51:12 +0300

    framework

--- simple-server/index.js
+++ simple-server/index.js
@@ -1,63 +1,71 @@
-const http = require('node:http');
-const fs = require('node:fs/promises');
+const http = require("node:http");
+const fs = require("node:fs/promises");
 
 const server = http.createServer();
 
-server.on('request', async (request, response) => {
-
-  const getHTMLFile = request.method === 'GET' && request.url === '/';
-  const getCSSFile = request.method === 'GET' && request.url === '/index.css';
-  const getJSFile = request.method === 'GET' && request.url === '/index.js';
+server.on("request", async (request, response) => {
+  const getHTMLFile = request.method === "GET" && request.url === "/";
+  const getCSSFile = request.method === "GET" && request.url === "/index.css";
+  const getJSFile = request.method === "GET" && request.url === "/index.js";
 
   if (getHTMLFile) {
-    response.setHeader('Content-Type', 'text/html');
+    response.setHeader("Content-Type", "text/html");
 
-    const fileHandle = await fs.open('./public/index.html', 'r');
+    const fileHandle = await fs.open("./public/index.html", "r");
     const fileStream = fileHandle.createReadStream();
 
     fileStream.pipe(response);
   }
 
   if (getCSSFile) {
-    response.setHeader('Content-Type', 'text/css');
+    response.setHeader("Content-Type", "text/css");
 
-    const fileHandle = await fs.open('./public/index.css', 'r');
+    const fileHandle = await fs.open("./public/index.css", "r");
     const fileStream = fileHandle.createReadStream();
 
     fileStream.pipe(response);
   }
 
   if (getJSFile) {
-    response.setHeader('Content-Type', 'text/javascript');
+    response.setHeader("Content-Type", "text/javascript");
 
-    const fileHandle = await fs.open('./public/index.js', 'r');
+    const fileHandle = await fs.open("./public/index.js", "r");
     const fileStream = fileHandle.createReadStream();
 
     fileStream.pipe(response);
   }
 
-  if (request.method === 'POST' && request.url === '/login') {
-    response.setHeader('Content-Type', 'application/json');
+  if (request.method === "POST" && request.url === "/login") {
+    response.setHeader("Content-Type", "application/json");
     response.statusCode = 200;
 
     response.end(JSON.stringify({ message: "You are loged in." }));
   }
 
-  if (request.method === 'POST' && request.url === '/upload') {
-    response.setHeader('Content-Type', 'application/json');
-    const fileHandle = await fs.open('./storage/image.png', 'w');
+  if (request.method === "PUT" && request.url === "/user") {
+    response.setHeader("Content-Type", "application/json");
+    response.statusCode = 401;
+
+    response.end(JSON.stringify({ message: "You first have to log in." }));
+  }
+
+  if (request.method === "POST" && request.url === "/upload") {
+    response.setHeader("Content-Type", "application/json");
+
+    const fileHandle = await fs.open("./storage/image.png", "w");
     const fileStream = fileHandle.createWriteStream();
 
     request.pipe(fileStream);
 
-    request.on('end', () => {
+    request.on("end", () => {
       response.statusCode = 200;
-      response.end(JSON.stringify({ message: "The file is uploaded succesfuly!" }));
+      response.end(
+        JSON.stringify({ message: "The file is uploaded succesfuly!" }),
+      );
     });
   }
-
 });
 
 server.listen(8000, () => {
-  console.log("First web server is listening")
-})
+  console.log("First web server is listening");
+});
---
+++ simple-web/server.js
@@ -0,0 +1,24 @@
+const Butter = require("./butter.js");
+const server = new Butter();
+
+const PORT = 9090;
+
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
+server.route("get", "/index.css", (req, res) => {
+  res.sendFile("./public/index.css", "text/css");
+});
+
+server.route("get", "/index.js", (req, res) => {
+  res.sendFile("./public/index.js", "text/javascript");
+});
+
+server.route("post", "/login", (req, res) => {
+  res.status(400).json({ message: "Bad login info." });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is on port ${PORT}`);
+});
